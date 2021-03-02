f3e77b0d7b9cf54c23de7fd39250e2858b6b9338
Barry Shovlin <bshovlin32@gmail.com>
2021-03-02 03:13:31 -0600

    tinkering with phase 3

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
 ﻿using System;
+using System.Threading;
 
 Main();
 
+
 void Main()
 {
+
     Console.WriteLine("Can you guess the secret number?");
     Console.WriteLine("--------------------------------");
 
-    string Guess = Console.ReadLine();
-    int GuessNumber = Int32.Parse(Guess);
-    int SecretNumber = 42;
-
-    if (GuessNumber == SecretNumber)
+    void Guess()
     {
-        Console.WriteLine("You got it!  Nice work!");
-    }
-    else
-    {
-        Console.WriteLine("Try again, ya big dumb babboon");
-    }
+        string Guess = Console.ReadLine();
+        int GuessNumber = Int32.Parse(Guess);
+        int SecretNumber = 42;
 
+        if (GuessNumber == SecretNumber)
+        {
+            Console.WriteLine("You got it!  Nice work!");
+        }
+        else
+        {
+            Console.WriteLine("Try again, ya big dumb babboon");
+        }
+    }
+    Guess();
 }
 
